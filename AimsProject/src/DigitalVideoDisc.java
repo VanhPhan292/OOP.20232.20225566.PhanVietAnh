388ef2febc2830e95e7c0397f37c5e7c71dfc470
VanhPhan292 <phan.viet.anh.292@gmail.com>
2024-03-25 11:47:48 +0700

    feat(aims): add basic code for aims project

---
+++ AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,25 @@
+
+public class DigitalVideoDisc {
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirectory() {
+		return directory;
+	}
+	public int getLenght() {
+		return lenght;
+	}
+	public float getCost() {
+		return cost;
+	}
+	private String title;
+	private String category;
+	private String directory;
+	private int lenght;
+	private float cost;
+
+}
+	
